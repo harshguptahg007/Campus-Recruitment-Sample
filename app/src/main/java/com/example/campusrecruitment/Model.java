68f0c236abc7dbbbfda8b8a02fff110341b1fc0e
Harsh Gupta <harshguptahg007@gmail.com>
2019-04-09 14:53:58 +0530

    initial commit

---
+++ app/src/main/java/com/example/campusrecruitment/Model.java
@@ -0,0 +1,14 @@
+package com.example.campusrecruitment;
+
+public class Model {
+
+    public String type;
+
+    public Model(){}
+
+    public Model(String type){
+
+        this.type = type;
+    }
+
+}
